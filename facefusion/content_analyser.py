4202fd64c847a570cd1d4e083afe9696a7e12d34
Barak Segal <barak353@gmail.com>
2024-07-12 21:02:33 +0300

    disable

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -48,15 +48,15 @@ def clear_content_analyser() -> None:
 
 
 def pre_check() -> bool:
-	download_directory_path = resolve_relative_path('../.assets/models')
-	model_url = MODELS.get('open_nsfw').get('url')
-	model_path = MODELS.get('open_nsfw').get('path')
-
-	if not state_manager.get_item('skip_download'):
-		process_manager.check()
-		conditional_download(download_directory_path, [ model_url ])
-		process_manager.end()
-	return is_file(model_path)
+	#download_directory_path = resolve_relative_path('../.assets/models')
+	#model_url = MODELS.get('open_nsfw').get('url')
+	#model_path = MODELS.get('open_nsfw').get('path')
+    #
+	#if not state_manager.get_item('skip_download'):
+	#	process_manager.check()
+	#	conditional_download(download_directory_path, [ model_url ])
+	#	process_manager.end()
+	return True
 
 
 def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
@@ -69,16 +69,16 @@ def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
 
 
 def analyse_frame(vision_frame : VisionFrame) -> bool:
-	content_analyser = get_content_analyser()
-	vision_frame = prepare_frame(vision_frame)
-
-	with conditional_thread_semaphore():
-		probability = content_analyser.run(None,
-		{
-			content_analyser.get_inputs()[0].name: vision_frame
-		})[0][0][1]
-
-	return probability > PROBABILITY_LIMIT
+	#content_analyser = get_content_analyser()
+	#vision_frame = prepare_frame(vision_frame)
+    #
+	#with conditional_thread_semaphore():
+	#	probability = content_analyser.run(None,
+	#	{
+	#		content_analyser.get_inputs()[0].name: vision_frame
+	#	})[0][0][1]
+    #
+	return False
 
 
 def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
@@ -91,24 +91,24 @@ def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
 @lru_cache(maxsize = None)
 def analyse_image(image_path : str) -> bool:
 	frame = read_image(image_path)
-	return analyse_frame(frame)
+	return analyse_frame(frame) if frame else True
 
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
-	video_frame_total = count_video_frame_total(video_path)
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(start_frame or 0, end_frame or video_frame_total)
-	rate = 0.0
-	counter = 0
-
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
-		for frame_number in frame_range:
-			if frame_number % int(video_fps) == 0:
-				frame = get_video_frame(video_path, frame_number)
-				if analyse_frame(frame):
-					counter += 1
-			rate = counter * int(video_fps) / len(frame_range) * 100
-			progress.update()
-			progress.set_postfix(rate = rate)
-	return rate > RATE_LIMIT
+	#video_frame_total = count_video_frame_total(video_path)
+	#video_fps = detect_video_fps(video_path)
+	#frame_range = range(start_frame or 0, end_frame or video_frame_total)
+	#rate = 0.0
+	#counter = 0
+    #
+	#with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
+	#	for frame_number in frame_range:
+	#		if frame_number % int(video_fps) == 0:
+	#			frame = get_video_frame(video_path, frame_number)
+	#			if analyse_frame(frame):
+	#				counter += 1
+	#		rate = counter * int(video_fps) / len(frame_range) * 100
+	#		progress.update()
+	#		progress.set_postfix(rate = rate)
+	return False
